67071e23762e22b30460a17d1c0c092d0b6d2039
Miouzora <alexandre.franquet@epitech.eu>
2024-05-14 00:00:55 +0200

    add pymunk

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import esper
+from pyray import *
+
+import pymunk  # Import pymunk..
+
+space = pymunk.Space()  # Create a Space which contain the simulation
+space.gravity = 0, -981  # Set its gravity
+
+body = pymunk.Body()  # Create a Body
+body.position = 50, 100  # Set the position of the body
+
+poly = pymunk.Poly.create_box(body)  # Create a box shape and attach to body
+poly.mass = 10  # Set the mass on the shape
+space.add(body, poly)  # Add both body and shape to the simulation
+
+print_options = pymunk.SpaceDebugDrawOptions()  # For easy printing
+
+for _ in range(100):  # Run simulation 100 steps in total
+    space.step(0.02)  # Step the simulation one step forward
+    space.debug_draw(print_options)
 
 from dataclasses import dataclass as component
 
@@ -16,6 +35,13 @@ print(player)
 
 def main():
     print("Hello world!")
+    init_window(800, 450, "Hello")
+    while not window_should_close():
+        begin_drawing()
+        clear_background(WHITE)
+        draw_text("Hello world", 190, 200, 20, VIOLET)
+        end_drawing()
+    close_window()
 
 
 if __name__ == "__main__":
