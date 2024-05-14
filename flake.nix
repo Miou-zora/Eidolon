b1abc849f59b7911b4d76a3c71005bf46702983f
Sigmanificient <edhyjox@gmail.com>
2024-05-15 00:19:58 +0200

    Declare python venv using the nix flake

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    esper = {
+      url = "github:benmoran56/esper?ref=v3.2";
+      flake = false;
+    };
   };
 
   outputs = {
@@ -14,13 +19,22 @@
     nixpkgs,
     flake-utils,
     pre-commit-hooks,
+    esper,
   }:
-    flake-utils.lib.eachSystem [
-      "x86_64-linux"
-    ]
+    flake-utils.lib.eachSystem ["x86_64-linux"]
     (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
+
+      py = {
+        env = pkgs.python311.withPackages (p: py.deps);
+        deps = with pkgs.python311.pkgs; [
+          grpcio
+          grpcio-tools
+          (self.packages.${system}.esper)
+          pymunk
+        ];
+      };
+    in rec {
       formatter = pkgs.alejandra;
 
       checks = let
@@ -37,15 +51,55 @@
         };
       };
 
-      devShells.default = pkgs.mkShell {
-        inherit (self.checks.${system}.pre-commit-check) shellHook;
-
+      devShells.default = pkgs.mkShell rec {
         name = "Eidolon";
+
         env.LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
-        packages = with pkgs; [
-          python312
-          python312Packages.black
-        ];
+
+        packages = with pkgs;
+          [
+            py.env
+            py.env.pkgs.venvShellHook
+          ]
+          ++ (with python312Packages; [
+            black
+          ])
+          ++ [pkgs.raylib];
+
+        venvDir = "venv";
+        shellHook = ''
+          ${checks.pre-commit-check.shellHook}
+
+          SOURCE_DATE_EPOCH=$(date +%s)
+
+          if [ -d "${venvDir}" ]; then
+            echo "Skipping venv creation, '${venvDir}' already exists"
+          else
+            echo "Creating new venv environment in path: '${venvDir}'"
+            ${py.env.python.interpreter} -m venv "${venvDir}"
+          fi
+
+          source "${venvDir}/bin/activate"
+          ${venvDir}/bin/pip install -r requirements.txt
+        '';
+
+        postVenvCreation = ''
+          pip install -r ./requirements.txt
+          pip install -e .
+        '';
+      };
+
+      packages = {
+        esper = pkgs.python311Packages.buildPythonPackage {
+          pname = "esper";
+          version = "3.2";
+          pyproject = true;
+
+          src = esper;
+
+          build-system = [py.env.pkgs.flit-core];
+          doCheck = false;
+        };
       };
     });
 }
