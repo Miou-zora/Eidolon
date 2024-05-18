a160b1e369e3261bfeb2ab745872ca8a729e982f
Miouzora <alexandre.franquet@epitech.eu>
2024-05-18 11:49:17 +0200

    Remove local use of processor in profit to esper builtin

--- common/engine/world.py
+++ common/engine/world.py
@@ -1,7 +1,7 @@
 import esper
 
 from .entity import EntityId
-from .processor import Processor
+from .processor import ProcessorClass
 from .resource_manager import ResourceManager
 from .time import TimeUnit
 
@@ -9,15 +9,13 @@ from .time import TimeUnit
 class World:
     def __init__(self, name: str):
         self.name = name
-        self.processors: list[Processor] = []
 
     @staticmethod
     def create_entity() -> EntityId:
         return esper.create_entity()
 
     def update(self, resource_manager: ResourceManager, elapsed_time: TimeUnit) -> None:
-        for processor in self.processors:
-            processor.process(resource_manager, elapsed_time)
+        esper.process(resource_manager, elapsed_time)
 
-    def add_processor(self, processor):
-        self.processors.append(processor)
+    def add_processor(self, processor: ProcessorClass) -> None:
+        esper.add_processor(processor)
