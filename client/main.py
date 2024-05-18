e6e53292d0f25efe42dbdc31468dbff0c858a4a0
Miouzora <alexandre.franquet@epitech.eu>
2024-05-18 16:58:59 +0200

    Create name component

--- client/main.py
+++ client/main.py
@@ -1,50 +1,29 @@
 import logging
 
-from client.processors.render_processor import RenderProcessor
-from client.processors.window_processor import WindowProcessor
-from client.resources.window_resource import WindowResource
+from common.components.name import Name
+from common.components.position import Position
 from common.engine.engine import Engine
 from common.engine.entity import Entity
-from common.engine.processor import Processor
-from common.engine.resource import Resource
-from common.engine.resource_manager import ResourceManager
+from common.processors.log_processor import LogProcessor
 from common.resources.time_providers import UnitTimeProvider
-from common.resources.time_providers.time_provider import TimeProvider
 
 logging.basicConfig(level=logging.NOTSET)
 logger = logging.getLogger(__name__)
 
 
-class TestResource(Resource):
-    def __init__(self):
-        self.a = 0
-
-
-class TestProcessor(Processor):
-    def __init__(self):
-        super().__init__()
-
-    def process(self, r: ResourceManager) -> None:
-        test_resource: TestResource = r.get_resource(TestResource)
-        time_provider: UnitTimeProvider = r.get_resource(TimeProvider)
-        if test_resource is None or time_provider is None:
-            raise NotImplemented(
-                f"Resource not found: TestResource:{test_resource} or UnitTimeProvider:{time_provider}"
-            )
-        test_resource.a += 1
-
-
 def run():
     engine: Engine = Engine()
 
-    engine.register_resource(TestResource())
     engine.register_resource(UnitTimeProvider())
-    engine.register_resource(WindowResource(title="Eidolon"))
-    engine.add_processor(WindowProcessor(engine))
-    engine.add_processor(RenderProcessor())
-    engine.add_processor(TestProcessor())
+    engine.add_processor(LogProcessor())
 
-    Entity()
+    entity: Entity = Entity()
+    entity.add_component(Position(32, 64))
+    entity.add_component(Name("First Entity"))
+    entity2: Entity = Entity()
+    entity2.add_component(Position(69, 42))
+    entity2.add_component(Name("Second Entity"))
+
     engine.run()
 
 
--- common/processors/log_processor.py
+++ common/processors/log_processor.py
@@ -2,10 +2,12 @@ import logging
 
 import esper
 
+from common.components.name import Name
 from common.components.position import Position
 from common.engine.processor import Processor
-from common.engine.resource import Resource
-from common.engine.time import TimeUnit
+from common.engine.resource_manager import ResourceManager
+from common.resources.time_providers import UnitTimeProvider
+from common.resources.time_providers.time_provider import TimeProvider
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +16,12 @@ class LogProcessor(Processor):
     def __init__(self):
         super().__init__()
 
-    def process(self, _: dict[str, Resource], elapsed_time: TimeUnit) -> None:
-        for ent, pos in esper.get_component(Position):
-            logger.debug(f"Entity {ent}: {pos}")
+    def process(self, r: ResourceManager) -> None:
+        time_provider: UnitTimeProvider = r.get_resource(TimeProvider)
+        if time_provider is None:
+            raise NotImplemented(
+                f"Resource not found: UnitTimeProvider:{time_provider}"
+            )
+        logger.info(f"Elapsed time: {time_provider.get_elapsed_time()}")
+        for ent, (pos, name) in esper.get_components(Position, Name):
+            logger.debug(f"{name}: {pos}")
