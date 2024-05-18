586792384fd2da928d83cbcd5d1a2afc5bac0a61
Sigmanificient <edhyjox@gmail.com>
2024-05-18 20:05:39 +0200

    Add physac package

--- flake.nix
+++ flake.nix
@@ -100,6 +100,36 @@
           build-system = [py.env.pkgs.flit-core];
           doCheck = false;
         };
+
+        physac = pkgs.stdenv.mkDerivation rec {
+          name = "physac";
+          version = "2.5-unstable-20240518";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "victorfisac";
+            repo = "Physac";
+            rev = "29d9fc06860b54571a02402fff6fa8572d19bd12";
+            hash = "sha256-PTlV1tT0axQbmGmJ7JD1n6wmbIxUdu7xho78EO0HNNk=";
+          };
+
+          dontBuild = true;
+          installPhase = ''
+            mkdir -p $out/{include,lib/pkgconfig}
+
+            cp $src/src/physac.h $out/include/physac.h
+
+            cat <<EOF > $out/lib/pkgconfig/physac.pc
+            prefix=$out
+            includedir=$out/include
+
+            Name: physac
+            Description: 2D physics header-only library for raylib
+            URL: https://github.com/victorfisac/Physac
+            Version: ${version}
+            Cflags: -I"{includedir}"
+            EOF
+          '';
+        };
       };
     });
 }
