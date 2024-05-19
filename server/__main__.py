95cc59bf5064fab925be9a4470a4d74610b801ee
huntears <huntears@kreog.com>
2024-05-19 15:53:17 +0200

    Add basic logger config in server

--- server/__main__.py
+++ server/__main__.py
@@ -1,16 +1,20 @@
 import logging
 import capnp
-import common.proto.global_capnp as augh
+import common.proto.global_capnp as gproto
 import threading
 import time
 import socketserver
+
+
+logging.basicConfig(level=logging.NOTSET)
+logger = logging.getLogger(__name__)
 
 
 class MyUDPHandler(socketserver.BaseRequestHandler):
     def handle(self) -> None:
         data = self.request[0]
         socket = self.request[1]
-        print(f"Got {len(data)} bytes from {self.client_address[0]}")
+        logger.debug(f"Got {len(data)} bytes from {self.client_address[0]}")
         socket.sendto(data, self.client_address)
 
 
@@ -26,9 +30,8 @@ def game_loop() -> None:
 
 
 if __name__ == "__main__":
-    logging.basicConfig()
     server_thread = threading.Thread(target=serve)
     server_thread.daemon = True
     server_thread.start()
-    logging.debug(f"Server loop running in thread: {server_thread.name}")
+    logger.debug(f"Server loop running in thread: {server_thread.name}")
     game_loop()
