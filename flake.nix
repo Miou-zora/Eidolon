43d75d5d4f3e8932dc5cad8c901052aa0767e5c7
Sigmanificient <edhyjox@gmail.com>
2024-05-21 05:51:53 +0200

    Remove unused esper flake input

--- flake.nix
+++ flake.nix
@@ -7,11 +7,6 @@
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    esper = {
-      url = "github:benmoran56/esper?ref=v3.2";
-      flake = false;
-    };
   };
 
   outputs = {
@@ -19,36 +14,32 @@
     nixpkgs,
     flake-utils,
     pre-commit-hooks,
-    esper,
   }:
-    flake-utils.lib.eachSystem ["x86_64-linux"] (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-        {
-          formatter = pkgs.alejandra;
+    flake-utils.lib.eachSystem ["x86_64-linux"]
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      {
+        formatter = pkgs.alejandra;
 
-          checks = let
-            hooks = {
-              alejandra.enable = true;
-              check-merge-conflicts.enable = true;
-              check-shebang-scripts-are-executable.enable = true;
-              check-added-large-files.enable = true;
-            };
-          in {
-            pre-commit-check = pre-commit-hooks.lib.${system}.run {
-              inherit hooks;
-              src = ./.;
-            };
+        checks = let
+          hooks = {
+            alejandra.enable = true;
+            check-merge-conflicts.enable = true;
+            check-shebang-scripts-are-executable.enable = true;
+            check-added-large-files.enable = true;
           };
-        }
-        // (let
-          outputs =
-            import ./nix
-            {inherit self pkgs system;};
         in {
-          devShells.default = outputs.shell;
-          packages = outputs.packages;
-        })
-    );
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            inherit hooks;
+            src = ./.;
+          };
+        };
+      }
+      // (let
+        outputs = import ./nix {inherit self pkgs system;};
+      in {
+        inherit (outputs) packages;
+        devShells.default = outputs.shell;
+      }));
 }
