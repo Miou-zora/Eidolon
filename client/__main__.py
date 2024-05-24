431374ac2e14c2c74528f677206841a8ce2f0ee4
Miouzora <alexandre.franquet@epitech.eu>
2024-05-25 00:34:58 +0200

    Add click processor to allow to make buttons

--- client/__main__.py
+++ client/__main__.py
@@ -10,17 +10,16 @@ from common.engine.entity import Entity
 from common.engine.plugin import Plugin
 from common.engine.processor import Processor
 from common.engine.schedule_label import ScheduleLabel
-from common.resources.time_providers.real_time_provider import RealTimeProvider
+from components.box_collider import BoxCollider
+from components.clickable import Clickable
 from components.controllable import Controllable
 from components.drawable import Drawable
 from components.speed import Speed
 from plugins.default_plugin import DefaultPlugin
-from plugins.scene_plugin import ScenePlugin
+from processors.click_processor import ClickProcessor
 from processors.connection_processor import ConnectionProcessor
 from processors.control_processor import ControlProcessor
 from resources.assets_manager import AssetsManager
-from resources.scene_manager import Scene
-from resources.scene_manager import SceneManager
 
 if TYPE_CHECKING:
     from common.engine.resource_manager import ResourceManager
@@ -41,41 +40,24 @@ class Setup(Processor):
 
         asset_name = "randomImage"
         asset_manager.load_texture(asset_name, f"assets/randomImage.png")
+        asset_size = asset_manager.get_texture_size(asset_name)
 
         network_manager.launch()
 
+        def TestClickableFunction():
+            TestClickableFunction.i += 1
+            logger.debug("Clicked %i", TestClickableFunction.i)
+
+        TestClickableFunction.i = 0
         _: Entity = Entity().add_components(
             Position(300, 300),
             Name("First Entity"),
             Drawable(asset_name),
+            Clickable(TestClickableFunction),
+            BoxCollider(asset_size[0], asset_size[1]),
             Controllable(),
             Speed(300),
         )
-
-
-class TestScene(Scene):
-    @staticmethod
-    def on_start() -> None:
-        logger.info("TestScene started")
-
-    @staticmethod
-    def on_exit() -> None:
-        logger.info("TestScene exited")
-
-
-class TestSceneProcessor(Processor):
-    def __init__(self):
-        super().__init__()
-        self.chrono = 0
-        self.time_to_change_scene = 5  # seconds
-
-    def process(self, r: ResourceManager) -> None:
-        scene_manager = r.get_resource(SceneManager)
-        time_provider = r.get_resource(RealTimeProvider)
-        self.chrono += time_provider.get_elapsed_time()
-        if self.chrono >= self.time_to_change_scene:
-            scene_manager.switch_to(TestScene)
-            self.chrono -= self.time_to_change_scene
 
 
 class ClientPlugin(Plugin):
@@ -88,10 +70,8 @@ class ClientPlugin(Plugin):
             # LogProcessor(),
             ControlProcessor(),
             ConnectionProcessor(),
-            TestSceneProcessor(),
-        ).insert_resources(
-            NetworkManager,
-        )
+            ClickProcessor(),
+        ).insert_resources(NetworkManager)
 
 
 def run():
@@ -100,7 +80,6 @@ def run():
     engine.add_plugins(
         DefaultPlugin().build(),
         ClientPlugin,
-        ScenePlugin,
     )
 
     engine.run()
