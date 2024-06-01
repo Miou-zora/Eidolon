9682934d52853b785b220bf9d760502eee8d22c9
Miouzora <alexandre.franquet@epitech.eu>
2024-06-02 01:01:27 +0200

    Use itertools combinations to check collisions

--- common/processors/collision_processor.py
+++ common/processors/collision_processor.py
@@ -1,4 +1,5 @@
 import logging
+from itertools import combinations
 
 import esper
 
@@ -13,58 +14,62 @@ from common.engine.resource_manager import ResourceManager
 logger = logging.getLogger(__name__)
 
 
+def component_generator():
+    for entity, (pos, mask, collider) in esper.get_components(
+        Position, CollisionMask, BoxCollider
+    ):
+        yield entity, (pos, mask, collider)
+
+
 class CollisionProcessor(Processor):
     def process(self, r: ResourceManager) -> None:
-        for ent, (pos, collisions, collision_mask, collider) in esper.get_components(
-            Position, Collisions, CollisionMask, BoxCollider
-        ):
-            collisions.collisions_between_entity = []
-            for iter_ent, (
-                iter_collision_mask,
-                iter_pos,
-                iter_box_collider,
-            ) in esper.get_components(CollisionMask, Position, BoxCollider):
-                if ent == iter_ent:
-                    continue
-                if not collision_mask.collide_with(iter_collision_mask):
-                    continue
-                if not CollisionProcessor.aabb_collision(
-                    pos, collider, iter_pos, iter_box_collider
-                ):
-                    continue
-                box_left = pos.x
-                box_right = pos.x + collider.x
-                box_top = pos.y
-                box_bottom = pos.y + collider.y
-                old_box_left = collisions.last_position.x
-                old_box_right = collisions.last_position.x + collider.x
-                old_box_top = collisions.last_position.y
-                old_box_bottom = collisions.last_position.y + collider.y
+        for (
+            (ent, (pos, collision_mask, collider)),
+            (iter_ent, (iter_pos, iter_collision_mask, iter_collider)),
+        ) in combinations(component_generator(), 2):
+            if not esper.has_component(ent, Collisions):
+                continue
+            collisions = esper.component_for_entity(ent, Collisions)
+            collisions.collisions_between_entity.clear()
+            if ent == iter_ent:
+                continue
+            if not collision_mask.collide_with(iter_collision_mask):
+                continue
+            if not CollisionProcessor.aabb_collision(
+                pos, collider, iter_pos, iter_collider
+            ):
+                continue
+            box_left = pos.x
+            box_right = pos.x + collider.x
+            box_top = pos.y
+            box_bottom = pos.y + collider.y
+            old_box_left = collisions.last_position.x
+            old_box_right = collisions.last_position.x + collider.x
+            old_box_top = collisions.last_position.y
+            old_box_bottom = collisions.last_position.y + collider.y
 
-                collided_from_left = old_box_right <= iter_pos.x < box_right
-                collided_from_right = (
-                    old_box_left >= iter_pos.x + iter_box_collider.x > box_left
+            collided_from_left = old_box_right <= iter_pos.x < box_right
+            collided_from_right = (
+                old_box_left >= iter_pos.x + iter_collider.x > box_left
+            )
+            collided_from_top = old_box_bottom <= iter_pos.y < box_bottom
+            collided_from_bottom = old_box_top >= iter_pos.y + iter_collider.y > box_top
+            if collided_from_top:
+                collisions.collisions_between_entity.append(
+                    Collision(iter_ent, CollisionDirection.DOWN)
                 )
-                collided_from_top = old_box_bottom <= iter_pos.y < box_bottom
-                collided_from_bottom = (
-                    old_box_top >= iter_pos.y + iter_box_collider.y > box_top
+            elif collided_from_bottom:
+                collisions.collisions_between_entity.append(
+                    Collision(iter_ent, CollisionDirection.UP)
                 )
-                if collided_from_top:
-                    collisions.collisions_between_entity.append(
-                        Collision(iter_ent, CollisionDirection.DOWN)
-                    )
-                elif collided_from_bottom:
-                    collisions.collisions_between_entity.append(
-                        Collision(iter_ent, CollisionDirection.UP)
-                    )
-                elif collided_from_left:
-                    collisions.collisions_between_entity.append(
-                        Collision(iter_ent, CollisionDirection.RIGHT)
-                    )
-                elif collided_from_right:
-                    collisions.collisions_between_entity.append(
-                        Collision(iter_ent, CollisionDirection.LEFT)
-                    )
+            elif collided_from_left:
+                collisions.collisions_between_entity.append(
+                    Collision(iter_ent, CollisionDirection.RIGHT)
+                )
+            elif collided_from_right:
+                collisions.collisions_between_entity.append(
+                    Collision(iter_ent, CollisionDirection.LEFT)
+                )
 
     @staticmethod
     def aabb_collision(
