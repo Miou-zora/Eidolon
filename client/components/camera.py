2aeecef4ec83672863a8568197f20f41f64904a9
Miouzora <alexandre.franquet@epitech.eu>
2024-06-17 13:53:16 +0200

    Remove useless init in Camera2D

--- client/components/camera.py
+++ client/components/camera.py
@@ -1,6 +1,7 @@
 from common.engine import component
-from common.utils.vector2 import Vector2
 from common.utils.debug import Debug
+from common.utils.vector2 import Vector2
+
 
 @component
 class Camera2D(Debug):
@@ -8,12 +9,3 @@ class Camera2D(Debug):
     rotation: float
     zoom: float
     id: int
-
-    def __init__(
-        self, offset: Vector2, rotation: float, zoom: float, _id: int = -1
-    ) -> None:
-        self.offset = offset
-        self.rotation = rotation
-        self.zoom = zoom
-        self.id = _id
-
