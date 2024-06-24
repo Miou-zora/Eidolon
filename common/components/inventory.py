9e3601a810e3cbfa76708d7866e8115d662cbe47
Miouzora <alexandre.franquet@epitech.eu>
2024-06-24 12:58:20 +0200

    Add typing for __check_cords method

--- common/components/inventory.py
+++ common/components/inventory.py
@@ -9,10 +9,16 @@ from common.utils.item.inventory_item import InventoryItem
 from common.utils.vector2 import Vector2
 
 if TYPE_CHECKING:
-    from typing import Callable, TypeVar
+    from typing import Protocol, TypeVar
 
     T = TypeVar("T")
-    PosFn = Callable[[int, int, ...], T | None]
+
+
+    class CheckCordMethod(Protocol):
+        # inv_instance == self in the Inventory class
+        def __call__(
+            self, inv_instance: Inventory, x: int, y: int, *args, **kwargs
+        ) -> T: ...
 
 
 @component
@@ -20,18 +26,14 @@ class Inventory:
     __size: Vector2
     __items: list[Optional[InventoryItem]] = field(default_factory=list)
 
-    # def __init__(self, size: Vector2):
-    #     self.__size = size
-    #     self.__items = [None] * int(size.x * size.y)
-
     def __post_init__(self):
-        self.__items = [None] * int(self.__size.x * self.__size.y)
+        self.__items = [None] * int(self.size.x * self.size.y)
 
     @staticmethod
-    def __check_cords(fn: Callable) -> Callable:
+    def __check_cords(fn: CheckCordMethod) -> CheckCordMethod:
         @wraps(fn)
-        def wrapper(self, x: int, y: int, *args, **kwargs):
-            if not (0 <= x < self.__size.x and 0 <= y < self.__size.y):
+        def wrapper(self, x: int, y: int, *args, **kwargs) -> T:
+            if not (0 <= x < self.size.x and 0 <= y < self.size.y):
                 return None
             return fn(self, x, y, *args, **kwargs)
 
@@ -41,12 +43,12 @@ class Inventory:
     def add_item(self, x: int, y: int, item: InventoryItem) -> Optional[
         InventoryItem]:
         item_before = self.get_item(x, y)
-        self.__items[int(x + y * self.__size.x)] = item
+        self.__items[int(x + y * self.size.x)] = item
         return item_before
 
     @__check_cords
     def get_item(self, x: int, y: int) -> Optional[InventoryItem]:
-        return self.__items[int(x + y * self.__size.x)]
+        return self.__items[int(x + y * self.size.x)]
 
     @property
     def size(self) -> Vector2:
@@ -54,8 +56,8 @@ class Inventory:
 
     @__check_cords
     def remove_item(self, x: int, y: int) -> Optional[InventoryItem]:
-        item = self.__items[int(x + y * self.__size.x)]
-        self.__items[int(x + y * self.__size.x)] = None
+        item = self.__items[int(x + y * self.size.x)]
+        self.__items[int(x + y * self.size.x)] = None
         return item
 
     def __str__(self) -> str:
